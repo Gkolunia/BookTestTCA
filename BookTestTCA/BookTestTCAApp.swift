6999b6b0d41bfd8f2eceff605e7b076919e59a83
Mykola <Gkolunia@gmail.com>
2025-02-05 11:27:54 +0200

    Initial commit with imported TCA framework

---
+++ BookTestTCA/BookTestTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTestTCAApp.swift
+//  BookTestTCA
+//
+//  Created by Mykola Hrybeniuk on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookTestTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookTestTCATests/BookTestTCATests.swift
@@ -0,0 +1,17 @@
+//
+//  BookTestTCATests.swift
+//  BookTestTCATests
+//
+//  Created by Mykola Hrybeniuk on 05.02.2025.
+//
+
+import Testing
+@testable import BookTestTCA
+
+struct BookTestTCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
